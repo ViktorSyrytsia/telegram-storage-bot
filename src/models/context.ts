b8c57ec22859d0e1227427108ec05858827f270a
Oleh-Viktor Syrytsia <syrytsiaviktor@gmail.com>
2022-04-13 21:41:47 +0300

    remove unneeded model

--- src/models/context.ts
+++
@@ -1,9 +0,0 @@
-import { Context } from "telegraf";
-import { Update } from "telegraf/typings/core/types/typegram";
-
-export interface ContextWithDB extends Partial<Context<Update>> {
-  db: {
-    token: string;
-    file_url: string;
-  };
-}
